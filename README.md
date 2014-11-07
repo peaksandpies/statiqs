eb164a898c82919a398c574ed5470429b6844ae8
Daniel Boekhoff <dboekhoff@gmail.com>
2014-11-07 20:08:22 +0100

    update readme, counting some sensor data

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
-=== statiqs
+### statiqs
 
 Small Web App to show sensor data or similar statistics in Realtime on a dashboard.
 
-=== Getting Started
+### WebApp
+
+
+
+### Python Scripts to read sensor data
+
+
+
+### RapsberryPi Details
+
+- WLAN Module
+- Bluetooth LE
+
+
+
+### Sensors
+
+- Temperature
+- Humidity
+- Magnetic
+- Distance
+- Movement
+- Lux
+
+
+
+### Where to get all the sensors
+
+Exp-Tech
+Adafruit
+Seeedstudio
+Watterott
+Tinkerforge
+
+
+
 
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,9 +3,14 @@ var socket = io.connect('http://localhost');
 
 
 socket.on('stats', function (data) {
-  console.log(data.value);
+  console.log( data.id + ": " + data.value);
 
-  $("#" + data.id + " p").html(data.value)
+  if (data.id == 'distance' || data.id == 'magnetic' || data.id == 'movement') {
+  	var current = $("#" + data.id + " p").text();
+  	$("#" + data.id + " p").html( +current + data.value);
+  } else {
+  	$("#" + data.id + " p").html(data.value);
+  }
 
   //socket.emit('frontend', { my: 'data' });
 });
